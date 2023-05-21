6cb1ea70fa111b47099517cc4d7c8a8e8f575f47
XanderWorld <xander.mymails@gmail.com>
2023-05-21 22:16:47 +0200

    configuration

---
+++ src/index.js
@@ -0,0 +1,19 @@
+require('dotenv/config');
+
+const {Client, IntentsBitField} = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        // IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on('ready', (c) => {
+    console.log(`🐇 | ${c.user.tag} is online!`)
+})
+
+
+client.login(process.env.TOKEN);
